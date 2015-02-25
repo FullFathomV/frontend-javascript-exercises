637515b0929262e8d9b0acd0818ca0510134288a
Stephen Watkins <steve@fullfathomv.com>
2015-02-25 11:27:13 -0800

    All debugging exercises completed.

--- 03-debugging-code/00_Reference_Error/challenge.js
+++ 03-debugging-code/00_Reference_Error/challenge.js
@@ -1,3 +1,6 @@
-// module.exports.hello = function(msg) {
-//   return "Hello " + msg;
-// };
+var hello = function(msg) {
+  console.log("hello " + msg);
+}
+module.exports.hello = function(msg) {
+return "Hello " + msg;
+};
--- 03-debugging-code/03_Console_Log/challenge.js
+++ 03-debugging-code/03_Console_Log/challenge.js
@@ -1,3 +1,6 @@
-module.exports.add = function(a,b) {
-  return a*b;
-};
+module.exports.add = function(a, b) {
+  console.log("a equals: "  + a);
+  console.log("b equals: "  + b);
+  console.log("a+b equals:"  + 3);
+  return a+b;
+}
